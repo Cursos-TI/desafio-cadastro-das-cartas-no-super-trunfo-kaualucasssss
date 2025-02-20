ffcc551df7697d77077ba55786879927e3399753
kaualucasssss <kauajg667@gmail.com>
2025-02-20 18:44:35 +0000

    OLA

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,35 +3,6 @@
 int main(){
 
 
-    char estado;
-    char codigodacarta[30];
-    char nomedacidade[30];
-    float area;
-    int populacao;
-    float pib;
-    float percapital;
-    int pontosturisticos;
-    
-    
-    
-    printf("carta: a01\n\n");
-    
-    printf("estado: A\n", estado);
-    printf("codigo da carta: A01 \n", codigodacarta);
-    printf("nome da cidade: sao paulo\n", nomedacidade);
-    printf("populacao :16000000 \n", populacao);
-    printf("area em km2: 248.219 km2\n", area);
-    printf("pib: R$ 400.26 bilhoes de reais\n",pib);
-    printf("numero de pontos turisticos: 36\n\n\n", pontosturisticos);
-    
-    
-    printf("estado: B\n", estado);
-    printf("codigo da carta: A02 \n", codigodacarta);
-    printf("nome da cidade: rio de janeiro\n", nomedacidade);
-    printf("populacao : 14000000 \n", populacao);
-    printf("area em km2: 43.750 km2\n", area);
-    printf("pib: R$ 400.34 bilhoes de reais\n",pib);
-    printf("numero de pontos turisticos: 20 \n\n\n", pontosturisticos);
 char estado;
 char codigodacarta[30];
 char nomedacidade[30];
@@ -43,7 +14,7 @@ int pontosturisticos;
 
 
 
-printf("carta: a01\n\n");
+printf("carta:A01\n\n");
 
 printf("estado: A\n", estado);
 printf("codigo da carta: A01 \n", codigodacarta);
@@ -53,6 +24,7 @@ printf("area em km2: 248.219 km2\n", area);
 printf("pib: R$ 400.26 bilhoes de reais\n",pib);
 printf("numero de pontos turisticos: 36\n\n\n", pontosturisticos);
 
+printf("carta: A02\n\n");
 
 printf("estado: B\n", estado);
 printf("codigo da carta: A02 \n", codigodacarta);
@@ -62,5 +34,6 @@ printf("area em km2: 43.750 km2\n", area);
 printf("pib: R$ 400.34 bilhoes de reais\n",pib);
 printf("numero de pontos turisticos: 20 \n\n\n", pontosturisticos);
 
-return 0;
+
+
 }
