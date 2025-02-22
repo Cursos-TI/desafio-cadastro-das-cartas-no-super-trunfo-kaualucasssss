9571075252224507d0ed8de7263defe8ef5371cc
kaualucasssss <kauajg667@gmail.com>
2025-02-22 17:18:02 +0000

    oia

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,37 +2,127 @@
 
 int main(){
 
+        char estado1, estado2;
+        char codigo1[4], codigo2[4];
+        char nome_cidade1[45], nome_cidade2[45];
+       unsigned long int populacao1, populacao2;
+        float  area1, area2;
+        float pib, pib2;
+        int num_ponto_turistico1, num_ponto_turistico2;
+        float divisao1, divisao2, divi3, divi4;
+        float total1, totaL2;
 
-char estado;
-char codigodacarta[30];
-char nomedacidade[30];
-float area;
-int populacao;
-float pib;
-float percapital;
-int pontosturisticos;
+         printf("---- CADASTRO CARTA 1 ----\n\n");
+                
+            printf("--Digite o estado (Uma letra de 'A' a 'b'): \n");
+            scanf(" %c", &estado1);
+                
+            printf("--Digite o código da cidade (A letra do estado e um número de 01 a 04): \n");
+            scanf("%s", codigo1);
+            printf("--Digite o nome da cidade: \n");
+            scanf(" %s[^\n]", nome_cidade1);
+    
+            printf("--Digite a população da cidade: \n");
+            scanf("%lu", &populacao1);
+    
+            printf("--Digite a area da cidade em KM2: \n");
+            scanf("%f", &area1);
+    
+            printf("--Digite o PIB da cidade em bilhões: \n");
+            scanf("%f", &pib);
+    
+            printf("--Digite o número de pontos turísticos: \n");
+            scanf("%d", &num_ponto_turistico1);
+
+           divisao1  = (populacao1 + area1) / 2;
+
+           divi3 = (pib+populacao1) / 2;
+           
+           
+            printf("---- CADASTRO CARTA 2 ----\n\n");
+                
+            printf("--Digite o estado (Uma letra de 'A' a 'b'): \n");
+            scanf(" %c", &estado2);
+                
+            printf("--Digite o código da cidade (A letra do estado e um número de 01 a 04): \n");
+            scanf("%s", codigo2);
+            getchar ();
+            printf("--Digite o nome da cidade: \n");
+            scanf(" %s[^\n]", nome_cidade2);
+    
+            printf("--Digite a população da cidade: \n");
+            scanf("%lu", &populacao2);
+    
+            printf("--Digite a area da cidade em KM2: \n");
+            scanf("%f", &area2);
+    
+            printf("--Digite o PIB da cidade em bilhões: \n");
+            scanf("%f", &pib2);
+    
+            printf("--Digite o número de pontos turísticos: \n");
+            scanf("%d", &num_ponto_turistico2);
+
+            divisao2  = (populacao2 + area2) / 2;
+
+            divi4 = (pib2+populacao2) / 2;
+           
+           
+             printf("\n--Carta 1--\n\n");
+            printf("-Estado: %c\n", estado1);
+            printf("-Código da cidade: %s\n", codigo1);
+            printf("-Nome da Cidade: %s\n", nome_cidade1);
+            printf("-População da cidade: %lu\n", populacao1);
+            printf("-Área da cidade em km²: %.2f KM²\n", area1);
+            printf("-PIB da cidade em bilhões de reais: %.2f bilhões de reais\n", pib);
+            printf("-Número de pontos turísticos: %d\n", num_ponto_turistico1);
+            printf("-Densidade populacional: %.2f hab/km²\n", divisao1);
+            printf("-PIB per capita: %.2f reais\n", divi3);
+
+
+        printf("\n--Carta 2--\n\n");
+            printf("-Estado: %c\n", estado2);
+            printf("-Código da cidade: %s\n", codigo2);
+            printf("-Nome da Cidade: %s\n", nome_cidade2);
+            printf("-População da cidade: %lu\n", populacao2);
+            printf("-Área da cidade em km²: %.2f KM²\n", area2);
+            printf("-PIB da cidade em bilhões de reais: %.2f bilhões de reais\n", pib2);
+            printf("-Número de pontos turísticos: %d\n", num_ponto_turistico2);
+            printf("-Densidade populacional: %.2f hab/km²\n", divisao2);
+            printf("-PIB per capita: %.2f reais\n", divi4);
 
 
 
-printf("carta:A01\n\n");
 
-printf("estado: A\n", estado);
-printf("codigo da carta: A01 \n", codigodacarta);
-printf("nome da cidade: sao paulo\n", nomedacidade);
-printf("populacao :16000000 \n", populacao);
-printf("area em km2: 248.219 km2\n", area);
-printf("pib: R$ 400.26 bilhoes de reais\n",pib);
-printf("numero de pontos turisticos: 36\n\n\n", pontosturisticos);
 
-printf("carta: A02\n\n");
 
-printf("estado: B\n", estado);
-printf("codigo da carta: A02 \n", codigodacarta);
-printf("nome da cidade: rio de janeiro\n", nomedacidade);
-printf("populacao : 14000000 \n", populacao);
-printf("area em km2: 43.750 km2\n", area);
-printf("pib: R$ 400.34 bilhoes de reais\n",pib);
-printf("numero de pontos turisticos: 20 \n\n\n", pontosturisticos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
